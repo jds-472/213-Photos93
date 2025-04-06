03bedeece05f264b756b74b108664e031a5a463a
jds-472 <jds472@scarletmail.rutgers.edu>
2025-04-05 21:39:00 -0400

    stock profile has images

--- PhotosFX/src/photosfx/Controller.java
+++ PhotosFX/src/photosfx/Controller.java
@@ -9,8 +9,6 @@ public class Controller {
     private Label label;
 
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        label.setText("Stock");
     }
 }
