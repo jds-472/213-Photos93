0072daa72a1a6cb9b32f1bcc227c5fed9d8c1ffe
jds-472 <jds472@scarletmail.rutgers.edu>
2025-04-07 22:20:06 -0400

    enter key on username works and dynamic options

--- PhotosFX/src/photosfx/LoginController.java
+++ PhotosFX/src/photosfx/LoginController.java
@@ -1,8 +1,6 @@
 package photosfx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,26 +8,28 @@ import javafx.scene.Node;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 
 public class LoginController {
 
     @FXML
     private TextField userField;
-    private StockController stockController = new StockController();
+
+    public void userFieldKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode().toString().equals("ENTER")) {
+            enter(new ActionEvent(userField, null));
+        }
+    }
 
     public void enter(ActionEvent event) {
-        if (event.getSource() == null) {
-            System.out.println("Event source is null");
-            return;
-        }
+        System.out.println("called");
         if (!userField.getText().isEmpty() && userField.getText().equals("stock")) {
             try {
                 Parent root = FXMLLoader.load(getClass().getResource("stock.fxml"));
                 Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 stage.setScene(new Scene(root));
                 stage.show();
-                //stockController.initialize();
             } catch (Exception e) {
                 e.printStackTrace();
             }
--- PhotosFX/src/photosfx/StockController.java
+++ PhotosFX/src/photosfx/StockController.java
@@ -6,28 +6,49 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+
 
 import java.util.ArrayList;
 
 public class StockController {
 
-    @FXML
-    private Label label;
-    private Image stock0;
-    private Image stock1;
-    private Image stock2;
-    private Image stock3;
-    private Image stock4;
-    private ArrayList<Image> stock = new ArrayList<>(java.util.Arrays.asList(stock0, stock1, stock2, stock3, stock4));
-    private ArrayList<Image> images = new ArrayList<>();
-    private HBox photoOptions = new HBox(5);
+    //@FXML
+    @FXML private Label label;
+    @FXML private ImageView stock0;
+    @FXML private ImageView stock1;
+    @FXML private ImageView stock2;
+    @FXML private ImageView stock3;
+    @FXML private ImageView stock4;
+    @FXML private Button photoOption0;
+    @FXML private Button photoOption1;
+    @FXML private Button photoOption2;
+    @FXML private Button photoOption3;
+    @FXML private HBox photoOptions;
+    @FXML private VBox leftBox;
+    private ArrayList<ImageView> stock = new ArrayList<>(java.util.Arrays.asList(stock0, stock1, stock2, stock3, stock4));
+    //private ArrayList<Image> Images = new ArrayList<>();
 
     public void initialize() {
         label.setText("Stock");
+    }
+
+    public void slideShow(ActionEvent event) {
+        leftBox.getChildren().remove(photoOptions);
+        leftBox.getChildren().addAll(photoOptions);
+        // try {
+        //     Parent root = FXMLLoader.load(getClass().getResource("slideshow.fxml"));
+        //     Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //     stage.setScene(new Scene(root));
+        //     stage.show();
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
     }
 
     public void logOut(ActionEvent event) {
