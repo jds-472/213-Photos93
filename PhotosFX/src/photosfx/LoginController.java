a933990c249b30a885bcabe3636a3667e8d7f34b
jds-472 <jds472@scarletmail.rutgers.edu>
2025-04-12 20:28:40 -0400

    fix

--- PhotosFX/src/photosfx/LoginController.java
+++ PhotosFX/src/photosfx/LoginController.java
@@ -11,8 +11,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import model.Data;
-import model.User;
-
 
 public class LoginController {
 
